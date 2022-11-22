f2d053eb37353c6b82b90dc7f8d2c4d17b9d0826
Lorenzo Vaccarecci <gekkeijuita@gmail.com>
2022-11-22 16:49:52 +0100

    changed to "run" according to the new version

--- main.py
+++ main.py
@@ -3,13 +3,13 @@ import platform
 
 # Get the OS and change the command accordingly
 system = platform.system();
-if system == 'Windows':
-    cmd = "echo Hello Windows"
+if system == "Windows":
+    cmd = "g++ -Wall -std=c++14 **.cpp -o a"
 elif system == 'Linux':
-    cmd = "echo Hello Linux"
+    cmd = "echo Not supported yet"
 else:
     cmd = "clang++ -Werror -Wno-error=unused-variable -Wall -W main.cpp"
 
 # Run the command and print the output
-returned_output = subprocess.check_output(cmd)
-print('Output:', returned_output.decode('utf-8'))
+returned_output = subprocess.run(cmd , stdout = subprocess.PIPE)
+returned_output.stdout.decode('utf-8')
