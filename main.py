80d5905e608166b8c92699f3d5bfe20215addfc8
Lorenzo Vaccarecci <gekkeijuita@gmail.com>
2022-11-25 15:31:18 +0100

    Check files in given directory

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-# Check the file that are in the same folder of the script for now
+# Check the file that are in a folder chosen by the user
 from tkinter import *
 from tkinter import filedialog
 
@@ -6,7 +6,9 @@ import subprocess
 import os
 import shutil
 import pathlib
+import platform
 
+exe_file = "a"
 
 def runprint_output(cmd):
     returned_output = subprocess.run(cmd , stdout = subprocess.PIPE)
@@ -16,15 +18,20 @@ def runprint_output(cmd):
     else:
         print("Correct")
 
-def check_files(file,system):
+def delete_file(file):
+    print("Deleting file: " + file)
+    os.remove(file)
+
+def check_file(file,system):
     cmd = "echo Nothing found"
     if system == "Windows":
-        cmd = 'g++ -Wall -std=c++14 ' + file +  ' -o a'
+        cmd = 'g++ -Wall -std=c++14 ' + file +  ' -o ' + exe_file
     elif system == 'Linux':
         cmd = "echo Not supported yet"
     else:
         cmd = "clang++ -Werror -Wno-error=unused-variable -Wall -W " + file
     runprint_output(cmd) 
+    delete_file(file)
 
 directorySrc = filedialog.askdirectory() # Ask the user to select the folder
 directoryDest = pathlib.Path(__file__).parent.resolve() # Get the path of the script
@@ -32,14 +39,13 @@ directoryDest = pathlib.Path(__file__).parent.resolve() # Get the path of the sc
 #copy all .cpp files in the same folder of the script
 for file in os.listdir(directorySrc):
     if file.endswith(".cpp"):
+        print("Copying " + file)
         shutil.copy2(directorySrc + "/" + file, directoryDest)
 
-# Check the files (to fix)
-#for file in os.listdir():
-#    if file.endswith(".cpp"):
-#        check_files(file,platform.system())
+# Check the files
+for file in os.listdir():
+    if file.endswith(".cpp"):
+        print("Checking " + file)
+        check_file(file,platform.system())
 
-# Delete all the files
-    for file in os.listdir():
-        if file.endswith(".cpp"):
-            os.remove(file)
+os.remove(exe_file + ".exe") 
